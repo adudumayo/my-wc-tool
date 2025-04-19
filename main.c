aeef04bca0493eafa0688722ca56f51872eab548
adudumayo023 <adudumayo023@student.wethinkcode.co.za>
2025-04-19 20:36:59 +0200

    First version of the byte returner function

--- main.c
+++ main.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    printf("Hey");
+void byteReturner(char* fileName) {
+    FILE* flptr;
+
+    flptr = fopen(fileName, "r");
+
+    if (flptr == NULL) {
+        printf("This file does not exist\n");
+    } else {
+        printf("We Up\n");
+    }
+} // Has to return the number of bytes in the given file
+
+int main(int argc, char **argv) {
+
+    byteReturner(argv[1]);
 }
